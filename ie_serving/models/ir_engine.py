b04f40399820951898756ac0b18b7a5d34bf46d5
AYOUB BOUZIANE <ayyoub.bouziane@gmail.com>
2020-11-11 00:29:12 +0100

    Update ir_engine.py
    .

--- ie_serving/models/ir_engine.py
+++ ie_serving/models/ir_engine.py
@@ -97,7 +97,7 @@ class IrEngine():
         with open(model_xml, mode='rb') as file:
             model_xml_file_bytes = file.read()
         with open(model_bin, mode='rb') as file:
-        model_bin_file_bytes = file.read()
+            model_bin_file_bytes = file.read()
         ie= IECore( )
         net = ie.read_network(model=model_xml_file_bytes, weights=model_bin_file_bytes, init_from_buffer = True)
         #        net = IENetwork(model=model_xml, weights=model_bin)
